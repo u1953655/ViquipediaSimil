806941f93a54e48ea59d14c977c9dd095592d0e1
villaret <mateu.villaret@udg.edu>
2020-10-28 16:46:47 +0100

    stopwords i xml example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,29 @@
-object Main extends App {
-  println("Hello, World!")
+import scala.xml.XML
+import scala.util.matching.Regex
+
+
+object tractaxml extends App {
+
+  val xmlleg=new java.io.InputStreamReader(new java.io.FileInputStream("32509.xml"), "UTF-8")
+  val xmllegg = XML.load(xmlleg)
+  // obtinc el titol
+  val titol=(xmllegg \\ "title").text
+  // obtinc el contingut de la pàgina
+  val contingut = (xmllegg \\ "text").text
+
+  // identifico referències
+  val ref = new Regex("\\[\\[[^\\]]*\\]\\]")
+  println("La pagina es: " + titol)
+  println("i el contingut: ")
+  println(contingut)
+  val refs=(ref findAllIn contingut).toList
+
+  // elimino les que tenen :
+  val kk = refs.filterNot(x=> x.contains(':'))
+
+  // caldrà eliminar-ne més?
+
+  for (r<-refs) println(r)
+  println(refs.length)
+  println(kk.length)
 }
